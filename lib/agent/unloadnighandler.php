c3379b6d6395a9fd1002c774a8ff38a76d17c852
slimic960 <slimic.tver@yandex.ru>
2024-07-31 16:12:27 +0300

    2.2.8

---
+++ lib/agent/unloadnighandler.php
@@ -0,0 +1,25 @@
+<?php
+namespace Eshoplogistic\Delivery\Agent;
+
+use \Bitrix\Main\Application,
+    \Bitrix\Main\Data\Cache,
+    \Eshoplogistic\Delivery\Config;
+
+/** Agents for cache managing
+ * Class UndloadingHandler
+ * @package Eshoplogistic\Delivery\Agent
+ */
+
+class UndloadingHandler
+{
+
+    /** Agent for clearing cache and managed cache directories
+     * @return string
+     */
+    public static function updateStatus()
+    {
+
+
+        return "Eshoplogistic\Delivery\Agent\UndloadingHandler::updateStatus();";
+    }
+}
