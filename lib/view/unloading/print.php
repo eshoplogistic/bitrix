8a49c71c494efa91c8c8b3ac000d5718761f131f
slimic960 <slimic.tver@yandex.ru>
2026-08-11 13:08:07 +0300

    4.0.55

--- lib/view/unloading/print.php
+++ lib/view/unloading/print.php
@@ -14,7 +14,7 @@ IncludeModuleLangFile(__FILE__);
 $request = \Bitrix\Main\Application::getInstance()->getContext()->getRequest();
 
 $SALE_RIGHT = $APPLICATION->GetGroupRight('sale');
-if ($SALE_RIGHT < 'R') {
+if ($SALE_RIGHT !== 'W') {
     $APPLICATION->AuthForm(GetMessage('ACCESS_DENIED'));
 }
 
